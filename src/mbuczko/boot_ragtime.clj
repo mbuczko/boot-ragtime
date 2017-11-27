8ff1ccd283ef75776cf10c48915ad123bb1e6fbc
Scott Nielsen <scottnielsen5@gmail.com>
2017-11-27 16:45:49 -0700

    Allow ragtime version to be specified as an option
    This bumps the default version from `0.5.2` to `0.7.2`.
    The motivating cause for this change is that older versions
    of `ragtime` do not work with newer versions of `clojure.java.jdbc`.

--- src/mbuczko/boot_ragtime.clj
+++ src/mbuczko/boot_ragtime.clj
@@ -5,21 +5,22 @@
    [boot.core   :as core]
    [boot.util   :as util]))
 
-(def ^:private rag-deps '[[ragtime/ragtime "0.5.2"]])
 
 (core/deftask ragtime
   "Apply/rollback ragtime migrations"
-  [d database  DATABASE   str  "database jdbc url"
-   g generate  MIGRATION  str  "name of generated migration."
-   m migrate              bool "Run all the migrations not applied so far."
-   r rollback             int  "number of migrations to be immediately rolled back."
-   l list-migrations      bool "List all migrations to be applied."
-   c driver-class  DRIVER str  "The JDBC driver class name to initialize."
-   _ directory DIRECTORY  str  "directory to store migrations in."]
+  [d database  DATABASE      str  "database jdbc url"
+   g generate  MIGRATION     str  "name of generated migration."
+   m migrate                 bool "Run all the migrations not applied so far."
+   r rollback                int  "number of migrations to be immediately rolled back."
+   l list-migrations         bool "List all migrations to be applied."
+   c driver-class  DRIVER    str  "The JDBC driver class name to initialize."
+   _ directory DIRECTORY     str  "directory to store migrations in."
+   _ ragtime-version VERSION str  "The version of ragtime to use. default: 0.7.2"]
 
-  (let [worker  (pod/make-pod (update-in (core/get-env) [:dependencies] into rag-deps))
+  (let [worker  (pod/make-pod (update-in (core/get-env) [:dependencies] into [['ragtime/ragtime ragtime-version]]))
         command (if rollback :rollback (if migrate :migrate))
-        migrations-dir (or directory "migrations")]
+        migrations-dir (or directory "migrations")
+        ragtime-version (or ragtime-version "0.7.2")]
 
     (core/with-pre-wrap [fs]
       (if generate
